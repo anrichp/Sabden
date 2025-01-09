64bb041644aa0a425b3ad58a23da6f328e2ffbe5
Anrich Potgieter <anrichp@gmail.com>
2025-01-09 22:41:30 +0000

    apply material to button

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, MatButtonModule],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <button mat-flat-button class="primary" type="button">Search</button>
       </form>
       <section class="results"></section>
       <app-housing-location></app-housing-location>
