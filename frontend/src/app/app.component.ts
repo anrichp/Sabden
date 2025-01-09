767927e02fb7ca98d850326988f8a52d93cdecad
Anrich Potgieter <anrichp@gmail.com>
2025-01-09 21:00:01 +0000

    add template to home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   template: `
     <main>
       <header class="brand-name">
@@ -22,5 +23,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Homes';
+  title = 'homes';
 }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,12 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+      <section class="results"></section>
+    </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
